d06ce2eae8a74a24582d9e61081c7d6e7c194ed5
developerscode <developerscode@yahoo.com>
2013-09-26 11:37:58 +0530

    first

---
+++ Javascript/Common.js
@@ -0,0 +1,51 @@
+﻿//Water Mark for Textboxes
+
+function watermark(inputId, text) {
+    var inputBox = document.getElementById(inputId);
+    if (inputBox.value.length > 0) {
+        if (inputBox.value == text)
+            inputBox.value = '';
+    }
+    else
+        inputBox.value = text;
+}
+
+(function ($) {
+    $.fn.extend({
+        inputWatermark: function () {
+            return this.each(function () {
+                // retrieve the value of the ‘placeholder’ attribute
+                var watermarkText = $(this).attr('placeholder');
+                var $this = $(this);
+                if ($this.val() === '') {
+                    $this.val(watermarkText);
+                    // give the watermark a translucent look
+                    $this.css({ 'opacity': '0.65' });
+                }
+
+
+
+                $this.blur(function () {
+                    if ($this.val() === '') {
+                        // If the text is empty put the watermark
+                        // back
+
+                        $this.val(watermarkText);
+                        // give the watermark a translucent look
+                        $this.css({ 'opacity': '0.65' });
+                    }
+                });
+
+
+
+                $this.focus(function () {
+                    if ($this.val() === watermarkText) {
+                        $this.val('');
+                        $this.css({ 'opacity': '1.0' });
+                    }
+                });
+            });
+        }
+    });
+
+})(jQuery);
